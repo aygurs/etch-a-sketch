9f078a88a0198cef2165bd3c4bb696a6db5aba43
ayd111n <leylaagursoy@hotmail.com>
2023-08-27 14:52:42 +0100

    Make the grid start at 16 x 16

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const gridSizeInput = document.querySelector('#gridSizeInput');
 const gridContainer = document.querySelector('.gridContainer');
 
+for (let i = 0; i < (16 * 16); i++) {
+        
+    const squareSize = 700 / 16;
+
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('gridSquare');
+    gridSquare.style.width = `${squareSize}px`;
+    gridSquare.style.height = `${squareSize}px`;
+    gridSquare.addEventListener('mouseenter', function() {
+        gridSquare.classList.add('hovered');
+    })
+    gridContainer.appendChild(gridSquare);
+}
+
 gridSizeInput.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         const gridSizeValue = parseInt(gridSizeInput.value);
